2243e745cf1e470d01a0102c5254a065182f75d4
Jim Xie <xiewenwei@sina.com>
2021-12-04 09:43:45 +0800

    update

--- src/jupyter.sh
+++ src/jupyter.sh
@@ -1,3 +1,3 @@
 source /data/iot/notebook/envs/ai/bin/activate
 cd /data/iot/notebook/src
-jupyter-notebook --notebook-dir=./ --ip='*' --port=8888 --allow-root
+jupyter-notebook --notebook-dir=./ --ip='*' --port=80 --allow-root
